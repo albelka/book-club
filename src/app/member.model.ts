4d4a3513ad96f0bb118ce24e5fa79718e6115c8d
Anne Belka <antastic461@gmail.com>
2017-01-27 08:56:29 -0800

    setup firebase, add ability to list members

---
+++ src/app/member.model.ts
@@ -0,0 +1,3 @@
+export class Member {
+  constructor(public name: string, public age: number, public favorite: string, public preference: string, public suggests: string) {}
+}
--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
+import { Router } from '@angular/router';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { MemberService } from '../member.service';
+
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css']
+  styleUrls: ['./members.component.css'],
+  providers: [MemberService]
 })
 export class MembersComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
 }
